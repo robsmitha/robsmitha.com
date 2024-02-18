6d5aaec2abe54f862725f11891a4b80d9c43a189
robsmitha <robsmitha94@gmail.com>
2024-02-18 00:03:01 -0500

    update UI

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,42 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+  <v-navigation-drawer
+      v-model="drawer"
+      location="right"
+      temporary
+  >
+    <v-list-item
+      prepend-icon="mdi-food"
+      title="Menu"
+    ></v-list-item>
 
-      Essentials Preset
-    </v-app-bar-title>
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-home-outline" title="Home" value="home"></v-list-item>
+      <v-list-item prepend-icon="mdi-information-outline" title="About" value="about"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <v-app-bar
+    clipped-right
+    app
+    flat
+  >
+  <v-app-bar-title>
+    <v-icon>mdi-hand-wave-outline</v-icon>
+  </v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
   </v-app-bar>
 </template>
 
-<script lang="ts" setup>
-  //
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      drawer: null,
+    }
+  }
+});
 </script>
